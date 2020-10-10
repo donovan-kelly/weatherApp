fdb2fb7ac9867abaa80d4859fb285de762c9bdb5
Donovan Kelly <donovankellyjr,gd@gmail.com>
2020-10-10 00:28:41 -0400

    deploy my weather app to github pages

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My react app</h1>
+      <p>Hello mannnnnnn wgar are you doing</p>
+    </div>
+  );
+}
+
+export default App;
